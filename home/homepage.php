82925df7aeda60eef6cbf31b8bf27ef0489d2762
Sushmita24680 <Sushmita246802@gmail.com>
2021-03-18 13:04:03 +0530

    created header
    Start with header

---
+++ home/homepage.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    <link rel="stylesheet" href="stylesheet.css">  
+    <title>Raag lyrics</title>
+  </head>
+  <body>
+  <header>
+  <li style="list-style:none;" class="nav1">
+  <ul> <img src="70a36980d5c84b66898f56109051a4a4.png" style="height:55px;padding-left:10px;"/>
+  </ul> 
+  <ul><div class="nav2"><li><ul>home</ul>
+  <ul> playlist</ul>
+  <ul>about us </ul></li></div></ul>
+  
+  <ul id="login"><button>login</button></ul>
+  </li>
+  </header>
+
+</html>
