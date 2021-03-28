9579192bb9e600e48b46de2121bb27c8a38c80f6
Sushmita24680 <Sushmita246802@gmail.com>
2021-03-28 18:48:49 +0530

    added php in header
    navigator,raag,footer,stylesheet

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="../CSS/foot.css">
+</head>
+<body>
+<div class="footer">
+  
+  <div class='right'><p>India</p> 
+  <p>© 2021 Raag <p></div>
+  <div class="left"><img src="../images/logo2.png" style="width:7rem;"></div>
+  <div class="content">
+  <ul>
+<li><a href="#login">Legal</a></li>
+<li disabled><a href="#login">|</a></li>
+<li><a href="#home">About</a></li>
+<li><a href="#contactus">Investors</a></li>
+</ul>
+</div>
+</div>
+
+
+</body>
+</html>
---
+++ includes/navigation.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/nav.css">
+</head>
+<body>
+
+<!-- navbar -->
+<ul>
+<img src="../images/logo2.png" style="width:7rem;">
+<li><a href="#login">Login/Sign Up</a></li>
+<li disabled><a href="#login">|</a></li>
+<li><a href="#home">Home</a></li>
+<li><a href="#contactus">Contact Us</a></li>
+<li><a href="#playlist">Playlist</a></li>
+</ul>
+
+</body>
+</html>
